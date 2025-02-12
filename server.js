acc98acdf5fb7630f175530941ebcde38f59abfe
Abhishek <abhiaskar.18@gmail.com>
2025-02-12 23:35:16 +0530

    HTML, CSS, Javascript and Express working together

--- server.js
+++ server.js
@@ -1,29 +1,11 @@
 const express = require("express");
 const app = express();
 
-//Middleware function to log requests
-app.use((req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-    next();
-})
-
-//without next() the code will not move past this line and none of the routes will be called or displayed
-//not even the home route
+// Middleware to serve static files from 'public' folder
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the Home Page! 🚀");
-});
-
-app.get("/about", (req, res) => {
-    res.send("This is the About Page!");
-});
-
-app.get("/contact", (req, res) => {
-    res.send("This is the Contact Page!");
-});
-
-app.get("/services", (req, res) => {
-    res.send("Our Services: Web Development, App Development!");
+    res.sendFile(__dirname + "/public/index.html"); // Send HTML file
 });
 
 app.listen(3000, () => {
